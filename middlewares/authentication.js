7f18d4e2b896ab1a88627bfde17a0819d20506e8
Animesh Singh <animeshraja998@gmail.com>
2022-09-27 20:46:40 +0530

    umla

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -4,6 +4,7 @@ import User from "../models/User.js";
 export const authenticate = async (req, res, next) => {
   try {
     const token = req.cookies["jwt"];
+    console.log(token);
     if (!token) {
       return res.status(403).json({error: "Please Login First"});
     }
