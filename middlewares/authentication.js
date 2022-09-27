4d2b1721e035ffde87d1d13d5147feeaa8d52596
Animesh Singh <animeshraja998@gmail.com>
2022-09-27 20:36:37 +0530

    ompa

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -3,9 +3,9 @@ import User from "../models/User.js";
 
 export const authenticate = async (req, res, next) => {
   try {
-    const token = req.cookies.jwt;
+    const token = req.cookies["jwt"];
     if (!token) {
-      return res.status(403).json("Please Login First");
+      return res.status(403).json({error: "Please Login First"});
     }
 
     const data = await jwt.verify(token, process.env.JWT_KEY);
@@ -13,6 +13,6 @@ export const authenticate = async (req, res, next) => {
     req.user = user;
     next();
   } catch (error) {
-    return res.status(401).json("Please Login First");
+    return res.status(401).json({error: "Please Login First"});
   }
 };
