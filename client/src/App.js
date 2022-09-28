82807bee6bfc44601ec0aac34b5ac9eb6af0ad0f
Animesh Singh <animeshraja998@gmail.com>
2022-09-28 13:15:28 +0530

    uuuu

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home/Home';
 import Profile from './pages/Profile/Profile';
 import Auth from './pages/Auth/Auth';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 
 const App = () => {
