327d372bf98643b2873915b09258cd3f5292ac03
Animesh Singh <animeshraja998@gmail.com>
2022-09-28 11:55:57 +0530

    helio

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -4,10 +4,10 @@ import User from "../models/User.js";
 export const authenticate = async (req, res, next) => {
   try {
     const token = req.cookies["jwt"];
-    console.log(`1 ${token}`);
+    // console.log(`1 ${token}`);
     console.log(`2 ${req.cookies}`)
-    console.log(`3 ${req.headers.cookies}`)
-    console.log(`4 ${req.headers.cookie}`)
+    // console.log(`3 ${req.headers.cookies}`)
+    // console.log(`4 ${req.headers.cookie}`)
     if (!token) {
       return res.status(403).json({error: "Please Login First"});
     }
